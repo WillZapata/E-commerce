04071b6d3b3933b3a04b169f7ac193cdeb61d734
WillZapata <will.iamzapata06@gmail.com>
2021-10-31 23:44:25 -0500

    actualizado

--- main.py
+++ main.py
@@ -170,10 +170,10 @@ def get_productos():
     productos = sql_resapi_select_productos()
     return jsonify( {"productos": productos} )
 
-@app.route('/productos')
-def productos():
-    productos = sql_select_productos()
-    return render_template("producto.html", productos=productos)
+@app.route('/usuarios')
+def usuario():
+    usuarios = sql_select_usuarios()
+    return render_template("usuario.html", usuarios=usuarios)
 
 def sql_connection():
     try:
@@ -191,6 +191,14 @@ def sql_select_productos():
     productos = cursorObj.fetchall()
     return productos
 
+def sql_select_usuario():
+    sql = strsql = "select * from productos WHERE id=?"
+    con = sql_connection()
+    cursorObj = con.cursor()
+    cursorObj.execute(sql)
+    usuario = cursorObj.fetchall()
+    return usuario
+
 def sql_resapi_select_productos():
     sql = strsql = "select * from productos"
     con = sql_connection()
@@ -199,3 +207,94 @@ def sql_resapi_select_productos():
     productos = cursorObj.fetchall()
     lista_productos = [ { "id_producto": producto[0], "id_categoria": producto[1], "nombre_producto": producto[2], "precio": producto[3], "cantidad": producto[4] } for producto in productos ]
     return lista_productos
+
+
+def sql_select_usuarios():
+    sql = strsql = "select * from usuarios"
+    con = sql_connection()
+    cursorObj = con.cursor()
+    cursorObj.execute(sql)
+    usuarios = cursorObj.fetchall()
+    return usuarios
+
+def select_usuario():
+    con = sql_connection()
+    cursorObj = con.cursor()
+    cursorObj.execute("SELECT nombre, usuario, correo, contraseña FROM usuarios WHERE id=?;", (nombre, usuario, correo, contraseña))
+    usuarios = cursorObj.fetchall()
+    lista_usuarios = [ { "id": id, "nombre": nombre, "usuario": usuario, "correo": correo, "contraseña": contraseña } ]
+    return lista_usuarios
+
+
+# @app.route('/delete/<id>/', methods=['GET', 'POST'])
+# def borrar_producto():
+#     id = request.args.get('id')
+#     sql_delete_usuario(id)
+#     flash("Usuario id = {} eliminado".format(id))
+#     return redirect(url_for('usuario'))
+
+@app.route('/delete/<id>/', methods=['GET', 'POST'])
+def borrar_usuario(id):
+    int(id)
+    con = sql_connection()
+    cursorObj = con.cursor()
+    cursorObj.execute("DELETE FROM usuarios WHERE id=?", (id,))
+    con.commit()
+    flash("Usuario Eliminado Exitosamente")
+    return redirect(url_for('usuario'))
+
+
+# def sql_delete_usuario(id):
+#     sql = "DELETE FROM usuarios WHERE id = '{}'".format(id)
+#     print(sql)
+#     con = sql_connection()
+#     cursorObj = con.cursor()
+#     cursorObj.execute(sql)
+#     con.commit()
+#     con.close()
+
+
+@app.route('/actualizar_usuario/<id>/', methods=['GET'])
+def actualizar_usuario(id):
+    print("Hola mundo")
+    con = sql_connection()
+    cursorObj = con.cursor()
+    cursorObj.execute("SELECT * FROM usuarios WHERE id=?;", (id,))
+    usu = cursorObj.fetchone()
+    # cursorObj.execute("UPDATE usuarios SET nombre = ?, usuario = ?, correo = ?, contraseña = ? WHERE id = ?", (nombre,usuario,correo,contraseña,id))
+    con.commit()
+    # flash("Usuario Actualizado Exitosamente")
+    return render_template("actualizar.html",usu=usu)
+
+@app.route('/editar/<id>/', methods=['GET', 'POST'])
+def editar_usuario(id):
+    if request.method == 'POST':
+        nombre = request.form['Nombre']
+        usuario = request.form['Usuario']
+        correo = request.form['email']
+        contraseña = request.form['password']
+        con = sql_connection()
+        cursorObj = con.cursor()
+        cursorObj.execute("UPDATE usuarios SET nombre = ?, usuario = ?, correo = ?, contraseña = ? WHERE id = ?", (nombre,usuario,correo,contraseña,id))
+        con.commit()
+        flash("Usuario Actualizado Exitosamente")
+        return redirect(url_for('usuario'))
+
+# con = sql_connection()
+#     cursorObj = con.cursor()
+#     cursorObj.execute("SELECT nombre, usuario, correo, contraseña FROM usuarios WHERE id=?;", (nombre, usuario, correo, contraseña))
+#     usuarios = cursorObj.fetchall()
+#     lista_usuarios = [ { "id": id, "nombre": nombre, "usuario": usuario, "correo": correo, "contraseña": contraseña } ]
+
+# def sql_edit_producto(id, nombre, precio, existencia):
+#     sql = "UPDATE Producto SET nombre = '{}', precio = '{}', existencia = '{}' WHERE id = '{}'".format(nombre, precio, existencia, id)
+#     print(sql)
+#     con = sql_connection()
+#     cursorObj = con.cursor()
+#     cursorObj.execute(sql)
+#     con.commit()
+#     con.close()
+
+@app.route('/actualizar/<id>', methods=['GET'])
+def actualizar():
+    return render_template("actualizar.html")
